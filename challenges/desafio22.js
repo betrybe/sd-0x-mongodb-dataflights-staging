6ec20f8121104a0f16e1cc21529b90f72761f9b1
Rodrigo G. Oliveira <rodrigo.gargani@betrybe.com>
2021-01-18 14:41:55 -0300

    fix(eslint)

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,9 +1,9 @@
 db.voos
   .find(
     {
-      'empresa.nome': { $in: ['DELTA AIRLINES', 'AMERICAN AIRLINES'] },
-      'aeroportoOrigem.sigla': 'SBGR',
-      'aeroportoDestino.sigla': 'KJFK',
+      "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+      "aeroportoOrigem.sigla": "SBGR",
+      "aeroportoDestino.sigla": "KJFK",
     },
     { _id: 0, vooId: 1 },
   )
